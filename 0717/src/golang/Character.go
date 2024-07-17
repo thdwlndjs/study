4297f1fa85acd0bad00d32552fcdf53ac97848c9
supporter <davidjw82@gmail.com>
2024-07-17 17:13:33 +0900

    ay

---
+++ 0717/src/golang/Character.go
@@ -0,0 +1,24 @@
+package golang
+
+import "fmt"
+
+func main() {
+	fmt.Println()
+}
+
+type Character struct {
+	name string
+}
+type funcion interface {
+	attack()
+	defend()
+	getHealth()
+	takeDamage(damage int)
+}
+
+func attack() {
+
+}
+func defend() {
+
+}
