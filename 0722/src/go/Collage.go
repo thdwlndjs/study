fb79f8d2db17050fc23b9ecb79459d604d5d0c7a
supporter <davidjw82@gmail.com>
2024-07-24 18:06:41 +0900

    hmm

--- 0722/src/go/Collage.go
+++ 0722/src/go/Collage.go
@@ -2,27 +2,31 @@ package main
 
 import "fmt"
 
-type Student struct {
-	name    string
-	grade   int
-	teacher string
+type Student interface {
+	getName() string
+	getGrade() int
+	getStudInfo()
+	getTeacherInfo() string
 }
 type Collage struct {
 	name    string
 	grade   int
 	teacher string
 	course  int
+	infor
 }
 type Mid struct {
 	name    string
 	grade   int
 	teacher string
+	infor
 }
 
 type infor interface {
 	getName() string
 	getTeacher() string
 	getgrade() int
+	getStudInfo(info infor)
 }
 
 func (m Mid) getName() string {
@@ -43,16 +47,19 @@ func (m Mid) getTeacher() string {
 	return "담임선생님: 손흥민"
 }
 func (c Collage) getTeacher() string {
-	return "지도 교수님: " + c.getTeacher() + "담임선생님: 손흥민"
+	return "지도 교수님: " + c.teacher + "담임선생님: 손흥민"
 }
 
-var mainA *Mid = &Mid{"홍길동", 2, "차범근"}
-var mainB *Collage = &Collage{"홍길동", 2, "차범근", 22}
-
-func (s Student) getStudInfo(info infor) {
+func getStudInfo(info infor) {
 	fmt.Println("Student 클래스의 getStdInfo() 호출")
 	fmt.Println("이름: ", info.getName(), " 학년: ", info.getgrade())
 }
-func (s Student) getTeacher(info infor) {
+func getTeacher(info infor) {
 	fmt.Println(info.getTeacher())
+}
+
+func main() {
+	var c Collage = Collage{name: "홍길동", grade: 2, teacher: "차범근", course: 22}
+	c.getStudInfo(c)
+
 }
---
+++ 0724/src/golang/Exceliz.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"path/filepath"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("2개 이상의 실행인수가 필요합니다.")
+		return
+	}
+	word := os.Args[1]
+	file := os.Args[2:]
+	PrintAllFiles(files)
+}
+func GetFileList(path string) ([]string, error) {
+	return filepath.Glob(path)
+}
+func PrintAllFiles(files []string) {
+	for _, path := range files {
+		filelist, err := GetFileList(path)
+		if err != nil {
+			fmt.Println("파일 경로가 잘못되었습니다. err:", err, "path", path)
+			return
+		}
+		fmt.Println("찾으시는 파일 리스트")
+		for _, name := range filelist {
+			fmt.Println(name)
+		}
+	}
+}
